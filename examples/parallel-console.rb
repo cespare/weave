acb1208ef700694ad60d2aae4c66dbff27535f17
Caleb Spare <cespare@gmail.com>
2013-02-09 17:33:55 -0800

    Update parallel console to use new features.

--- examples/parallel-console.rb
+++ examples/parallel-console.rb
@@ -5,7 +5,7 @@ require "weave"
 
 usage = <<-EOS
 Usage:
-  $ ./parallel-ssh user@host1 user@host2 ...
+  $ ruby parallel-console.rb user@host1 user@host2 ...
 EOS
 
 abort usage if ARGV.empty?
@@ -13,12 +13,20 @@ abort usage if ARGV.empty?
 $stty_state = `stty -g`.chomp
 $pool = Weave.connect(ARGV)
 
-while command = Readline.readline(">>> ", true)
+prompt = ">>> "
+while command = Readline.readline(prompt, true)
+  prompt = ">>> "
   break unless command # ctrl-D
   command.chomp!
   next if command.empty?
   break if ["exit", "quit"].include? command
-  $pool.execute { run command }
+  bad_exit = false
+  $pool.execute do
+    result = run(command, :continue_on_failure => true)
+    bad_exit = result[:exit_code] && result[:exit_code] != 0
+    bad_exit ||= result[:exit_signal]
+  end
+  prompt = "!!! " if bad_exit
 end
 
 $pool.disconnect!
